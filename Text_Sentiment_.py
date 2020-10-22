8dfa352e46bf24485e6c785d4a13f032b12f5e42
Hrudayangam Mehta <51739491+Hrudayangam@users.noreply.github.com>
2020-10-23 02:52:10 +0530

    Add files via upload

---
+++ Text_Sentiment_.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from google_drive_downloader import GoogleDriveDownloader as gdd
+from torch.utils.data import DataLoader, Dataset
+from sklearn.feature_extraction.text import CountVectorizer
+from tqdm import tqdm, tqdm_notebook
+
+
+
+import nltk
+#nltk.download()
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device
+
+DATA_PATH = 'data/imdb_reviews.csv'
+if not Path(DATA_PATH).is_file():
+    gdd.download_file_from_google_drive(
+        file_id='1zfM5E6HvKIe7f3rEt1V2gBpw5QOSSKQz',
+        dest_path=DATA_PATH,
+    )
+
+
+
+class Sequences(Dataset):
+    def __init__(self, path):
+        df = pd.read_csv(path)
+        self.vectorizer = CountVectorizer(stop_words='english', max_df=0.99, min_df=0.005)
+        self.sequences = self.vectorizer.fit_transform(df.review.tolist())
+        self.labels = df.label.tolist()
+        self.token2idx = self.vectorizer.vocabulary_
+        self.idx2token = {idx: token for token, idx in self.token2idx.items()}
+        
+    def __getitem__(self, i):
+        return self.sequences[i, :].toarray(), self.labels[i]
+    
+    def __len__(self):
+        return self.sequences.shape[0]
+
+
+dataset = Sequences(DATA_PATH)
+train_loader = DataLoader(dataset, batch_size=4096)
+
+print(dataset[5][0].shape)
+
+
+class BagOfWordsClassifier(nn.Module):
+    def __init__(self, vocab_size, hidden1, hidden2):
+        super(BagOfWordsClassifier, self).__init__()
+        self.fc1 = nn.Linear(vocab_size, hidden1)
+        self.fc2 = nn.Linear(hidden1, hidden2)
+        self.fc3 = nn.Linear(hidden2, 1)
+    
+    def forward(self, inputs):
+        x = F.relu(self.fc1(inputs.squeeze(1).float()))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+model = BagOfWordsClassifier(len(dataset.token2idx), 128, 64)
+model
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad], lr=0.001)
+
+
+model.train()
+train_losses = []
+for epoch in range(100):
+    progress_bar = tqdm_notebook(train_loader, leave=False)
+    losses = []
+    total = 0
+    for inputs, target in progress_bar:
+        model.zero_grad()
+
+        output = model(inputs)
+        loss = criterion(output.squeeze(), target.float())
+        
+        
+        loss.backward()
+              
+        nn.utils.clip_grad_norm_(model.parameters(), 3)
+
+        optimizer.step()
+        
+        progress_bar.set_description(f'Loss: {loss.item():.3f}')
+        
+        losses.append(loss.item())
+        total += 1
+     
+    epoch_loss = sum(losses) / total
+    train_losses.append(epoch_loss)
+        
+    tqdm.write(f'Epoch #{epoch + 1}\tTrain Loss: {epoch_loss:.3f}')
+              
+
+
+def predict_review(text):
+    model.eval()
+    with torch.no_grad():
+        test_vector = torch.LongTensor(dataset.vectorizer.transform([text]).toarray())
+
+        output = model(test_vector)
+        prediction = torch.sigmoid(output).item()
+
+        if prediction > 0.5:
+            #print("the final result is:")
+            return (' Positive Review')
+        else:
+            #print("the final result is:")
+            return(" Negative Review")
+
+#text='good movie'
+#predict_review(text)
---
+++ chat_bot_gui_final.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 00:59:33 2020
+
+@author: hruda
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 02:54:42 2020
+
+@author: Asus
+
+from Text_Sentiment import *
+
+
+
+okay = "bunty is bad negative ew"
+
+
+
+"""
+
+
+
+
+#Creating GUI with tkinter
+
+
+from Text_Sentiment_ import predict_review
+
+
+import tkinter
+from tkinter import *
+
+
+
+
+
+
+def chatbot_response(msg):
+    res =  predict_review(msg)
+    
+    return res
+
+
+
+
+def send():
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+    
+
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "DRS-AI: " + "Please enter what you want to analyse:" + '\n\n')
+        
+        
+        ChatLog.insert(END, "You: " + msg+ '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+    
+        res = chatbot_response(msg)
+        ChatLog.insert(END, "DRS-AI: " + res + '\n\n')
+           
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+        
+        
+ 
+
+base = Tk()
+base.title("DRS-AI")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="cyan", height="8", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+                    command= send )
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+scrollbar.place(x=376,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+
+base.mainloop()
+
+
+
